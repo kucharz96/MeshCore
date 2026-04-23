8b5d7ec8ac76b35c51849928a5341a34fb8f5193
kucharz96 <33964778+kucharz96@users.noreply.github.com>
2026-04-23 23:42:29 +0200

    Restore startup reason after deep sleep wake on Cardputer

--- variants/m5stack_cardputer/M5CardputerBoard.h
+++ variants/m5stack_cardputer/M5CardputerBoard.h
@@ -34,6 +34,15 @@ public:
     delay(10);
     M5.In_I2C.writeRegister8(0x43, 0x01, 0xFF, 100000);
     delay(200);
+
+    esp_reset_reason_t reason = esp_reset_reason();
+    if (reason == ESP_RST_DEEPSLEEP) {
+      uint64_t wakeup_source = esp_sleep_get_ext1_wakeup_status();
+      if (wakeup_source & (1ULL << P_LORA_DIO_1)) {
+        startup_reason = BD_STARTUP_RX_PACKET;
+      }
+      rtc_gpio_deinit((gpio_num_t)P_LORA_DIO_1);
+    }
   }
 
   uint16_t getBattMilliVolts() override {
@@ -65,11 +74,11 @@ public:
   }
 
   void enterDeepSleep(uint32_t secs, int pin_wake_btn) {
-    esp_sleep_enable_ext1_wakeup((1ULL << P_LORA_DIO_1), ESP_EXT1_WAKEUP_ANY_HIGH);
-
+    uint64_t wake_mask = (1ULL << P_LORA_DIO_1);
     if (pin_wake_btn >= 0) {
-      esp_sleep_enable_ext1_wakeup((1ULL << pin_wake_btn) | (1ULL << P_LORA_DIO_1), ESP_EXT1_WAKEUP_ANY_HIGH);
+      wake_mask |= (1ULL << pin_wake_btn);
     }
+    esp_sleep_enable_ext1_wakeup(wake_mask, ESP_EXT1_WAKEUP_ANY_HIGH);
 
     if (secs > 0) {
       esp_sleep_enable_timer_wakeup(secs * 1000000ULL);
